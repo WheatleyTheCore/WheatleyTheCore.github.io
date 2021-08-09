fdd717202c06e795adc9c396f09ef0d4b7b5a1dd
WheatleyTheCore <alex.colwell@icloud.com>
2021-08-09 18:27:17 -0400

    changed greeter text and component, fixed some styles

--- src/components/greeter.js
+++ src/components/greeter.js
@@ -1,21 +1,17 @@
 import React from 'react'
-import Typewriter from 'typewriter-effect'
+import Typist from 'react-typist'
 
 const Greeter = () => {
   return (
     <div className="segment" id="greeter">
-      <Typewriter
-        options={{
-          delay: 70,
-          cursor: "_"
-        }}
-        onInit={(typewriter) => {
-          typewriter.typeString('Hi!')
-                    .pauseFor(700)
-                    .typeString(' My name is Alex.')
-                    .start();
-  }}
-      />
+      <Typist className="greeterText">
+        <Typist.Delay ms={300}/>
+        <span>Hi,</span>
+        <Typist.Delay ms={700}/>
+        <span> my name is Alex.</span>
+        <Typist.Delay ms={900}/>
+        <span> You can learn more about me <a href="#about">here</a>, about my skills <a href="#about2">here</a>, and find me elsewhere <a href="#about3">here</a>.</span>
+      </Typist>
     </div>
   )
 }
