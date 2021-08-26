837f27fd15bc116e4cbf9140893c3c84e09ee128
WheatleyTheCore <alex.colwell@icloud.com>
2021-08-26 12:34:08 -0400

    updated previewnum rules

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,24 +3,37 @@ import { graphql } from "gatsby";
 import ArticleSegment from "../components/articleSegment";
 import ProjectSegment from "../components/projectSegment";
 import GreeterSegment from "../components/greeterSegment";
-import AboutSegment from '../components/aboutSegment'
+import AboutSegment from "../components/aboutSegment";
 import Container from "../components/container";
 import MainNavbar from "../components/mainNavbar";
-import SEO from '../components/seo'
+import SEO from "../components/seo";
 
 // markup
 const IndexPage = ({ data }) => {
-  const isBrowser = typeof window !== "undefined"
-  const previewNum = isBrowser ? (window.innerWidth > 768 ? Math.floor(2 *(window.innerWidth / (300))) : 4) : 6;
+  const isBrowser = typeof window !== "undefined";
+  let previewNum = isBrowser
+    ? window.innerWidth > 768
+      ? Math.floor(2 * (window.innerWidth / 300))
+      : 4
+    : 6;
+  if (previewNum > 8) previewNum = 8;
 
   return (
     <Container>
-      <SEO title="Alex Colwell" description="Blog and Portfolio Site"/>
+      <SEO title="Alex Colwell" description="Blog and Portfolio Site" />
       <MainNavbar />
-      <GreeterSegment id="greeter"  />
-      <ProjectSegment id="projectSegment"  edges={data.allDatoCmsProject.edges} previewNum={previewNum}/>
-      <ArticleSegment id="blogSegment"  edges={data.allDatoCmsArticle.edges} previewNum={previewNum}/>
-      <AboutSegment  id="about"/>
+      <GreeterSegment id="greeter" />
+      <ProjectSegment
+        id="projectSegment"
+        edges={data.allDatoCmsProject.edges}
+        previewNum={previewNum}
+      />
+      <ArticleSegment
+        id="blogSegment"
+        edges={data.allDatoCmsArticle.edges}
+        previewNum={previewNum}
+      />
+      <AboutSegment id="about" />
     </Container>
   );
 };
